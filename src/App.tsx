8c22e535fdac9a1e61d5e99cae986f75897163ac
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-05 23:30:53 -0300

    Configure store & get store state

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Provider } from 'react-redux';
+
+import { Catalog } from './components/Catalog';
+import { store } from './store';
+
+export function App() {
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  );
+}
---
+++ src/components/Catalog.tsx
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux';
+
+export function Catalog() {
+  const catalog = useSelector((state) => state);
+  console.log(catalog);
+
+  return <h1>Catalog</h1>;
+}
