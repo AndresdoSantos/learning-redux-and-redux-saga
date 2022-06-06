ccdc6cd72d63f8591ed433cb93d48807264f7a67
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-05 23:49:08 -0300

    Separating modules and creating rootReducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import rootReducer from './modules/rootReducer';
+
 export const store = configureStore({
-  reducer: () => [],
+  reducer: rootReducer,
 });
