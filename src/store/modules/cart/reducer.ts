d95eb0a28cf9a879671a3b1b9e835a2d4a2fbf86
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-06 21:33:19 -0300

    Initial state of the reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,3 +1,13 @@
-export function cart() {
-  return [];
-}
+import { Reducer } from 'redux';
+
+import { ICartState } from './type';
+
+const INITIAL_STATE: ICartState = {
+  items: [],
+};
+
+const cart: Reducer<ICartState> = () => {
+  return INITIAL_STATE;
+};
+
+export { cart };
