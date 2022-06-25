64dc0d82f3992a57d17210ab8dc1e6db821f8434
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-25 14:36:33 -0300

    feat(sagas): use action to check stock

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,13 +1,35 @@
 /** Cada ação que pode acontecer dentro do Reducer vai ser uma função */
 import { IProduct } from './type';
 
-function addProductToCart(product: IProduct) {
+function addProductToCartRequest(product: IProduct) {
   return {
-    type: 'ADD_PRODUCT_TO_CART',
+    type: 'ADD_PRODUCT_TO_CART_REQUEST',
     payload: {
       product,
     },
   };
 }
 
-export { addProductToCart };
+function addProductToCartSuccess(product: IProduct) {
+  return {
+    type: 'ADD_PRODUCT_TO_CART_SUCCESS',
+    payload: {
+      product,
+    },
+  };
+}
+
+function addProductToCartFailure(productId: number) {
+  return {
+    type: 'ADD_PRODUCT_TO_CART_FAILURE',
+    payload: {
+      productId,
+    },
+  };
+}
+
+export {
+  addProductToCartRequest,
+  addProductToCartFailure,
+  addProductToCartSuccess,
+};
--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,9 +1,20 @@
-import { all, select, takeLatest } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
+import { all, call, put, select, takeLatest } from 'redux-saga/effects';
+
 import { IState } from '../..';
+import { api } from '../../../services/api';
+import {
+  addProductToCartFailure,
+  addProductToCartRequest,
+  addProductToCartSuccess,
+} from './actions';
 
-import { addProductToCart } from './actions';
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
-type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+type IStockResponse = {
+  id: number;
+  quantity: number;
+};
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
@@ -15,7 +26,18 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
     );
   });
 
-  console.log('Add to cart');
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
 }
 
-export default all([takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)]);
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock),
+]);
