62f0b89512ded0d798e3ad22f31b31bd2c19f04f
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-25 14:17:00 -0300

    feat(rootSaga): configuring redux-saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 
 import { ICartState } from './modules/cart/type';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
   cart: ICartState;
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (e) => middlewares,
 });
+
+sagaMiddleware.run(rootSaga);
